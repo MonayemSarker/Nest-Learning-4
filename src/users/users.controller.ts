fef0ab44126adcbb63bc49576bf1c2132e875119
MonayemSarker <bsse1228@iit.du.ac.bd>
2024-01-14 10:18:40 +0600

    Interceptor to hide password

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,9 @@
-import {Body, Controller, Post, Get, Patch, Param, Query, Delete } from '@nestjs/common';
+import {Body, Controller, Post, Get, Patch, Param, Query, Delete, } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
+
 @Controller('auth')
 export class UsersController {
     constructor(private userService: UsersService){}
@@ -13,11 +15,13 @@ export class UsersController {
         
     }
 
+    @UseInterceptors(ClassSerializerInterceptor)
     @Get(':id')
     findUser(@Param('id') id: string){
         return this.userService.findOne(parseInt(id));
     }
 
+    @UseInterceptors(ClassSerializerInterceptor)
     @Get('')
     findAll(@Query('email') email: string){
         return this.userService.find(email);
